2a9e1c7aff9ca595358f98073c6f2b0f008eca4e
Travis Puryear <travispuryear678@gmail.com>
2021-10-15 18:14:29 -0400

    inquirer first prompt in index working and printing to console.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-const Manager = require("./lib/Manager.js")
-const Engineer = require('./lib/Engineer.js')
-const Intern = require('./lib/Intern.js')
+const Manager = require("../lib/Manager")
+const Engineer = require('../lib/Engineer')
+const Intern = require('../lib/Intern')
 // module for questions 
 const inquirer = require('inquirer')
 const fs = require('fs')
+const { log } = require("console")
 
-startGetRole(){
-    inquirer.prompt([
+startGetRole = () => {
+    inquirer .prompt([
         {
-            type: 'input'
-            name: 'name'
-        }
+            type: 'input',
+            name: 'name',
+            message: 'Enter the managers name'
+            // Validate write later
+        },
 
+     
 
+    ])
+.then(function(answer){
+    console.log(answer);
 
-    ]
-    )
-
+})
 
 }
+startGetRole()
